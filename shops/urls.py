2a48a0d987532769186c0f69554308a2fd55d8b2
mobin <mobin@gmail.com>
2024-10-02 09:32:53 +0330

    continue organizing and managing project pages

--- shops/urls.py
+++ shops/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 from . import views
+from .views import login_user,logout_user
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="Home"),    
+    path("about/", views.about, name="About"),
+    path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout")
+    ]
--- shops/views.py
+++ shops/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def index(request):
     all_products = Product.objects.all()
-    return render(request, "index.html" , {'products': all_products})
+    return render(request, "index.html", {"products": all_products})
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("با موفقیت وارد شدید"))
+            return redirect("Home")
+        else:
+            messages.success(request, ("مشکلی در لاگین بوجود اومد دوباره امتحان کنید"))
+            return redirect("login")
+    else:
+        return render(request, "loginout.html")
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("خروج موفقیت آمیز"))
+    return redirect("Home")
